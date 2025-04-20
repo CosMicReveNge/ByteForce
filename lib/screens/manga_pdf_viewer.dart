9f69ceaecfe3aac0fc6ee62c73da02ed08956051
Vedant Agarwal <vedantagarwal13@gmail.com>
2025-04-20 10:49:35 +0530

    views

--- lib/screens/manga_pdf_viewer.dart
+++ lib/screens/manga_pdf_viewer.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_pdfviewer/pdfviewer.dart';
 
-class MangaPdfViewer extends StatelessWidget {
+enum ReadingMode { vertical, ltr, rtl }
+
+class MangaPdfViewer extends StatefulWidget {
   final String pdfAssetPath;
 
   const MangaPdfViewer({
@@ -10,12 +12,59 @@ class MangaPdfViewer extends StatelessWidget {
   });
 
   @override
+  State<MangaPdfViewer> createState() => _MangaPdfViewerState();
+}
+
+class _MangaPdfViewerState extends State<MangaPdfViewer> {
+  ReadingMode _readingMode = ReadingMode.vertical;
+
+  void _showReadingModeSelector() {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) => Column(
+        mainAxisSize: MainAxisSize.min,
+        children: ReadingMode.values.map((mode) {
+          return ListTile(
+            title: Text(mode.toString().split('.').last.toUpperCase()),
+            onTap: () {
+              setState(() {
+                _readingMode = mode;
+              });
+              Navigator.pop(context);
+            },
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  Widget _buildViewer() {
+    Widget pdfViewer = SfPdfViewer.asset(widget.pdfAssetPath);
+
+    if (_readingMode == ReadingMode.rtl) {
+      return Transform(
+        alignment: Alignment.center,
+        transform: Matrix4.rotationY(3.14159),
+        child: pdfViewer,
+      );
+    } else {
+      return pdfViewer;
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Manga Reader'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.view_agenda),
+            onPressed: _showReadingModeSelector,
+          ),
+        ],
       ),
-      body: SfPdfViewer.asset(pdfAssetPath),
+      body: _buildViewer(),
     );
   }
 }
